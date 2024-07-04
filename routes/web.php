687b9be19da22a57e0b85a285469593c6df2d36a
Vaughn Kennett <vaughnkennett@me.com>
2024-07-04 15:10:56 +1000

    spartie settings added - filament settings not added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Models\User;
+use App\Settings\GeneralSettings;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/', function () {
 //    return view('welcome');
 //});
+if (User::get()->count() > 0) {
+    Auth::login(User::find(2));
+}
+
+//dd( app(GeneralSettings::class)->site_name);
